793673c678562d60d51eb0757a5afc610ded6c0f
Dexv <arazrstmy500+g@gmail.com>
2025-05-27 19:53:49 +0200

    Update jsdomruntime.py

--- dexvstuff/jsdomruntime.py
+++ dexvstuff/jsdomruntime.py
@@ -11,11 +11,15 @@ class JsdomRuntime:
             }
         ).getInternalVMContext()
         
-    def eval(self, data: str, promise: bool = False, byte_array: bool = False) -> str:
-        script = (
-            f"{data}.then(r => JSON.stringify(Array.from(r)))" if byte_array and promise else
-            f"JSON.stringify(Array.from(({data})))" if byte_array else
-            f"{data}.then(r => JSON.stringify(r))" if promise else data
-        )
-        result = self.vm(script).runInContext(self.runtime)
-        return json.loads(result) if byte_array else result
+    def eval(self, data: str, promise: bool = False, byte_array: bool = False, suppress: bool = False) -> str:
+        try:
+            script = (
+                f"{data}.then(r => JSON.stringify(Array.from(r)))" if byte_array and promise else
+                f"JSON.stringify(Array.from(({data})))" if byte_array else
+                f"{data}.then(r => JSON.stringify(r))" if promise else data
+            )
+            result = self.vm(script).runInContext(self.runtime)
+            return json.loads(result) if byte_array else result
+        except Exception as e:
+            if suppress: pass
+            else: raise e
